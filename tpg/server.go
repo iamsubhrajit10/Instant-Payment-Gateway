c1adccff3fd504b457f3da2f5829a4ecf2cf5eaf
Aman Gupta <aman.gupta@iitgn.ac.in>
2024-04-12 23:57:58 +0530

    clean: unnecessary comments

--- tpg/server.go
+++ tpg/server.go
@@ -37,39 +37,3 @@ func main() {
 	s.Start()
 	e.Logger.Fatal(e.Start(":8080"))
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"time"
-// 	"tpg/scheduler"
-
-// 	"github.com/go-co-op/gocron/v2"
-// )
-
-// func main() {
-// 	s, err := gocron.NewScheduler()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	j, err := s.NewJob(
-// 		gocron.DurationJob(
-// 			10*time.Second,
-// 		),
-// 		gocron.NewTask(
-// 			scheduler.Reverse,
-// 		),
-// 	)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	fmt.Println(j.ID())
-// 	s.Start()
-// 	err = s.Shutdown()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// }
